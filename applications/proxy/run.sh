85aa5088ce3171ba8f0c6b12fc5ec4429a5ebce7
Lily Tsai <tslilyai@gmail.com>
2023-08-10 15:11:54 -0400

    Update run.sh
    tester and not root

--- applications/proxy/run.sh
+++ applications/proxy/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 db="myclass_cryptdb"
-mysql -uroot -ppass --execute='DROP DATABASE IF EXISTS '$db'; CREATE DATABASE '$db';'
+mysql -utester -ppass --execute='DROP DATABASE IF EXISTS '$db'; CREATE DATABASE '$db';'
 RUST_LOG=error cargo run --release &
