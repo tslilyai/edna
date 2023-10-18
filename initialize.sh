bfc155d8689441177aedca07cd5c439ca14023a8
tslilyai <tslilyai@gmail.com>
2023-10-18 10:40:37 -0400

    update initialize to use rust 1.70

--- initialize.sh
+++ initialize.sh
@@ -5,8 +5,9 @@ sudo apt update
 yes | sudo apt install libboost-all-dev libantlr3c-dev build-essential libglib2.0-dev docker.io python3-pip
 yes | sudo apt install texlive texlive-latex-extra texlive-fonts-recommended dvipng cm-super
 yes | sudo apt remove rustc
-yes | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh #take the default options
+nohup curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
+rustup default 1.70.0
 pip install matplotlib
 
 # move files around to the blockstore with enough room...
